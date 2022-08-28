43c0a1757784a5c0f1ac8d0766d967316b3cf782
ikarimullin <habibula>
2022-08-28 16:25:39 +0300

    EGISZTMK-546

--- src/pacman/App.java
+++ src/pacman/App.java
@@ -28,7 +28,7 @@ public class App extends Application {
     
     public static final float DTS_DIV_TS = (float) DRAWING_TILE_SIZE / TILE_SIZE;
     
-    public static final int CANVAS_INIT_WIDTH       = 400;
+    public static final int CANVAS_INIT_WIDTH       = 401;
     public static final int CANVAS_INIT_HEIGHT      = 400;
     
     private static final int WINDOW_WIDTH_OFFSET    = 6;
